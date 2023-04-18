87ad08167a2c21bf69ebb6a27adb5ab57d2abdd1
jelle <jelle@LAPTOP-PAJM4OP9>
2023-04-18 12:06:08 +0200

    3.1 testing your code

---
+++ ControllerTest/Model_Competition_NextTrackShould.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControllerTest
+{
+
+    [TestFixture]
+    public class Model_Competition_NextTrackShould
+    {
+        Competition? _competition { get; set; }
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            _competition = new Competition();
+        }
+
+        [Test]
+        public void NextTrack_EmptyQueue_ReturnNull()
+        {
+            var result = _competition.NextTrack();
+            Assert.IsNull(result);
+        }
+    }
+}
