9548b65e37b5902ca47a769027be0fb5c9147733
jelle <jelle@LAPTOP-PAJM4OP9>
2023-04-18 10:49:31 +0200

    2-6 visualization 1

--- Controller/Race.cs
+++ Controller/Race.cs
@@ -9,7 +9,7 @@ namespace Controller
 {
     public class Race
     {
-        public Track? Track { get; set; }
+        public Track Track { get; set; }
         public List<IParticipant>? Participants { get; set; }
         public DateTime StartTime { get; set; }
         private Random? _random;
--- RaceSimulatorRedux/Program.cs
+++ RaceSimulatorRedux/Program.cs
@@ -1,7 +1,17 @@
 ﻿using Controller;
 
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
 Data.Initialize();
 Data.NextRace();
+Console.WriteLine("Current track name:");
+Console.WriteLine(Data.CurrentRace.Track.Name);
+
+for (; ; )
+{
+    Thread.Sleep(100);
+}
+
+
+
+
